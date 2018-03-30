fdc9d0cf87e79864357159a02d6d4b7e05a453a8
newt.off <newt.off@kakaocorp.com>
2018-03-30 14:35:27 +0900

    clean up for next chapter

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,23 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './webpack.svg';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-
-  console.log(Icon);
-  element.appendChild(myIcon);
-  
-  console.log(Data);
 
   return element;
 }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,34 +5,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
   }
 };
