a9d0acc34a58231364defa9301b6e5876f41dcde
Junyoung Lee <junyoung.plum@gmail.com>
2018-03-31 00:40:03 +0900

    start babel

---
+++ babel/lib/greetings.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var name = 'John';
+var mood = 'Happy';
+
+console.log('Hey ' + name + ', are you feeling ' + mood + ' today?');
---
+++ babel/src/greetings.js
@@ -0,0 +1,4 @@
+let name = 'John';
+const mood = 'Happy';
+
+console.log(`Hey ${name}, are you feeling ${mood} today?`);
